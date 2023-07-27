3c9daf7a6829bb544551925b023e3f14d456cfbe
rtbk91 <72060263+rtbk91@users.noreply.github.com>
2023-07-28 09:25:36 +1000

    Add default types to variables
    Adding types to variable declarations and tidying the formatting

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "status" {
-  description = "Whether AWS Computer Optimizer should be Active or Inactive."
-  #type        = string
-  default = "Active"
+  description     = "Whether AWS Computer Optimizer should be Active or Inactive."
+  type            = string
+  default         = "Active"
   validation {
     condition     = contains(["Active", "Inactive"], var.status)
     error_message = "Value has to be Active or Inactive."
@@ -9,7 +9,8 @@ variable "status" {
 }
 
 variable "scope_name" {
-  description = "The name of the scope. It can be Organization|AccountId|ResourceArn"
+  description     = "The name of the scope. It can be Organization|AccountId|ResourceArn"
+  type            = string
   validation {
     condition     = var.scope_name == null ? true : (var.scope_name == "Organization" || var.scope_name == "AccountId" || var.scope_name == "ResourceArn")
     error_message = "Value has to be from the list [Organization,AccountId,ResourceArn]."
@@ -17,9 +18,9 @@ variable "scope_name" {
 }
 
 variable "scope_value" {
-  description = "The value of the scope. It should be ALL_ACCOUNTS if scope_name is Organization, vales of Account ID or resource arn as mentioned in scope name"
-
-  default = null
+  description     = "The value of the scope. It should be ALL_ACCOUNTS if scope_name is Organization, vales of Account ID or resource arn as mentioned in scope name"
+  type            = string
+  default         = null
   validation {
     condition     = var.scope_value == null ? true : (var.scope_value == "ALL_ACCOUNTS" || length(var.scope_value) == 12 || can(regex("^arn:aws:(?:ec2|autoscaling):[a-z]-[a-z]+-[1-9]:[[:digit:]]{12}:(.*)$", var.scope_value)))
     error_message = "Invalid scope_name and scope_value pair."
@@ -27,9 +28,9 @@ variable "scope_value" {
 }
 
 variable "resource_type" {
-
-  description = "The target resource type of the recommendation preference to create."
-  default     = null
+  type            = string
+  description     = "The target resource type of the recommendation preference to create."
+  default         = null
   validation {
     condition     = var.resource_type == null ? true : (var.resource_type == "Ec2Instance" || var.resource_type == "AutoScalingGroup" || var.resource_type == "EbsVolume" || var.resource_type == "LambdaFunction" || var.resource_type == "NotApplicable")
     error_message = "Value has to be from the list :[Ec2Instance,AutoScalingGroup,EbsVolume,LambdaFunction,NotApplicable]."
@@ -37,9 +38,9 @@ variable "resource_type" {
 }
 
 variable "enhanced_monitoring" {
-
-  description = "The status of the enhanced infrastructure metrics recommendation preference to make it Active or Inactive"
-  default     = null
+  type            = string
+  description     = "The status of the enhanced infrastructure metrics recommendation preference to make it Active or Inactive"
+  default         = null
   validation {
     condition     = var.enhanced_monitoring == null ? true : (var.enhanced_monitoring == "Active" || var.enhanced_monitoring == "Inactive")
     error_message = "Value has to be Active or Inactive."
